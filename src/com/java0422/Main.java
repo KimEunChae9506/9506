60b9fd639a4f90035b52f5fcef41d5cfa5dcce75
GD3 <GD3@DESKTOP-NUPJU7B>
2019-04-22 17:46:00 +0900

    안뇽

---
+++ src/com/java0422/Main.java
@@ -0,0 +1,29 @@
+package com.java0422;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+   int a = 1;
+   int b = 2;
+   String f = "사과가 ";
+   String t = "개 있다.";
+		   System.out.println(f + (a+b) + t);
+		  
+		 float c = 2.2f;
+		 
+		 System.out.println(c);
+		  
+		String 연산 = ("a+b= ");
+				System.out.println(연산 + (a+b));
+				
+				System.out.println("사과가 ");
+				
+		String 첫 = "첫";
+		String 둘 = " 둘";
+		System.out.println(첫);
+		System.out.println(둘);
+	}
+	
+
+}
